db3eb3236e15050922a879e8065aac713c85030a
Kevin See <Kevin.See@merck.com>
2020-01-09 13:42:05 -0800

    calculated pHOS on TRT scale using HNC estimates of hatchery abunance.

--- hnc_dabom/summarise_dabom.R
+++ hnc_dabom/summarise_dabom.R
@@ -12,6 +12,11 @@ library(lubridate)
 library(STADEM)
 library(DABOM)
 library(jagsUI)
+library(readxl)
+
+
+# source some needed functions
+source('R/definePopulations.R')
 
 #-------------------------------
 spp = 'Steelhead'
@@ -51,3 +56,44 @@ sthd_hnc_summ = 2017:2019 %>%
 sthd_hnc_summ %>%
   filter(!is.na(cv)) %>%
   write_csv('hnc_dabom/ModelFits/Sthd_HNC_abund.csv')
+
+# group things by population
+pop_df = definePopulations(spp)
+
+sthd_hnc_trt = sthd_hnc_summ %>%
+  left_join(pop_df) %>%
+  filter(!is.na(TRT)) %>%
+  mutate_at(vars(TRT),
+            list(fct_explicit_na)) %>%
+  mutate_at(vars(Year),
+            list(as.numeric)) %>%
+  group_by(Year, Species, TRT) %>%
+  summarise(est_hnc = sum(mean),
+            sd_hnc = sqrt(sum(sd^2)),
+            cv_hnc = sd_hnc / est_hnc) %>%
+  filter(est_hnc > 0)
+
+sthd_wild_trt = read_excel("../SnakeBasinFishStatus/Abundance_results/LGR_AllSummaries_Steelhead.xlsx",
+                           sheet = "Pop Total Esc") %>%
+  select(Year = spawn_yr,
+         Species = species,
+         TRT,
+         est_wild = mean,
+         sd_wild = sd, 
+         cv_wild = cv)
+
+sthd_hnc_trt %>%
+  inner_join(sthd_wild_trt) %>%
+  rowwise() %>%
+  mutate(pHOS = est_hnc / (est_wild + est_hnc),
+         pHOS_se = msm::deltamethod(~ x1 / (x1 + x2),
+                                    mean = c(est_hnc, est_wild),
+                                    cov = diag(c(sd_hnc, sd_wild)^2)),
+         pHOS_cv = pHOS_se / pHOS) %>%
+  ungroup() %>%
+  select(Year:TRT, 
+         HNC = est_hnc,
+         Wild = est_wild,
+         starts_with("pHOS")) %>%
+  write_csv('hnc_dabom/ModelFits/Sthd_HNC_pHOS.csv')
+  
